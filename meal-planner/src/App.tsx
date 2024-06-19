61f15dd68ff0bcefd1f7f9ebf25b37e53cec0c87
clarkanastasia <clarkanastasia@outlook.com>
2024-06-19 16:09:14 +0100

    feat: add route

--- meal-planner/src/App.tsx
+++ meal-planner/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import RecipeList from './RecipeList'
+import AddRecipe from './AddRecipe'
 
 const App = () => {
 
@@ -8,6 +9,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path = '/' element={<RecipeList />}/>
+        <Route path = '/addrecipe' element={<AddRecipe />} />
       </Routes>
     </Router>
   )
